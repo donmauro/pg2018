e1e77d3f2f93e8cf54d7673d8fda185f94a431b8
donmauro <cardella.maurizio@gmail.com>
2018-11-13 13:15:43 +0100

    hallo

--- app.js
+++ app.js
@@ -3,12 +3,13 @@ const express = require('express')
 const mountRoutes = require('./routes')
 
 const app = express()
+app.use(express.static( __dirname + '/public'))
 mountRoutes(app)
 
-let port = process.env.PORT; //HEROKU PORT
-if (port == null || port == "") {
-  port = 3000;
-}
+const port = process.env.PORT||3000 
+
+
+
 
 app.listen(port,  () => {
   console.log(`App listening on port ${ port }`);
--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,9 @@ const users = require('./users')
 const citta = require('./citta')
 
 module.exports = (app) => {
+  app.get( '/', ( req, res ) => {
+    res.sendfile('index.html');
+  })
   app.use('/users', users)
   app.use('/citta', citta)
 }
